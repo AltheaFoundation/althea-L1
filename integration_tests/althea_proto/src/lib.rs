37d2586ab15c4726840cefa19a8e1989e84af9d0
Christian Borst <christianoborst@gmail.com>
2023-01-19 12:11:20 -0500

    Fix lockup proto module architecture

--- integration_tests/althea_proto/src/lib.rs
+++ integration_tests/althea_proto/src/lib.rs
@@ -1,5 +1,5 @@
 //! This crate provides althea-chain proto definitions in Rust and also re-exports cosmos_sdk_proto for use by downstream
-//! crates. By default around a dozen proto files are generated and places into the prost folder. We could then proceed
+//! crates. By default around a dozen proto files are generated and placed into the prost folder. We could then proceed
 //! to fix up all these files and use them as the required dependencies for the proto files, but we chose instead to replace
 //! those paths with references ot upstream cosmos-sdk-proto and delete the other files. This reduces cruft in this repo even
 //! if it does make for a somewhat more confusing proto generation process.
