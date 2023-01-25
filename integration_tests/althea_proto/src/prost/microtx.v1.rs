11a7535c82ea44d89984edd8c1e3c1b2a74e125b
Christian Borst <christianoborst@gmail.com>
2023-01-25 12:31:37 -0500

    Add microtx query endpoints

--- integration_tests/althea_proto/src/prost/microtx.v1.rs
+++ integration_tests/althea_proto/src/prost/microtx.v1.rs
@@ -1,3 +1,137 @@
+/// Params struct
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Params {
+    #[prost(uint64, tag="1")]
+    pub xfer_fee_basis_points: u64,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct GenesisState {
+    #[prost(message, optional, tag="1")]
+    pub params: ::core::option::Option<Params>,
+}
+/// Query the current microtx params
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct QueryParamsRequest {
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct QueryParamsResponse {
+    #[prost(message, optional, tag="1")]
+    pub params: ::core::option::Option<Params>,
+}
+/// Query the additional fee paid on MsgXfer, determined by governance
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct QueryXferFeeRequest {
+    #[prost(uint64, tag="1")]
+    pub amount: u64,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct QueryXferFeeResponse {
+    #[prost(uint64, tag="1")]
+    pub fee_amount: u64,
+}
+/// Generated client implementations.
+pub mod query_client {
+    #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
+    use tonic::codegen::*;
+    /// Query defines the possible queries to make of the microtx module
+    #[derive(Debug, Clone)]
+    pub struct QueryClient<T> {
+        inner: tonic::client::Grpc<T>,
+    }
+    impl QueryClient<tonic::transport::Channel> {
+        /// Attempt to create a new client by connecting to a given endpoint.
+        pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
+        where
+            D: std::convert::TryInto<tonic::transport::Endpoint>,
+            D::Error: Into<StdError>,
+        {
+            let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
+            Ok(Self::new(conn))
+        }
+    }
+    impl<T> QueryClient<T>
+    where
+        T: tonic::client::GrpcService<tonic::body::BoxBody>,
+        T::Error: Into<StdError>,
+        T::ResponseBody: Body<Data = Bytes> + Send + 'static,
+        <T::ResponseBody as Body>::Error: Into<StdError> + Send,
+    {
+        pub fn new(inner: T) -> Self {
+            let inner = tonic::client::Grpc::new(inner);
+            Self { inner }
+        }
+        pub fn with_interceptor<F>(
+            inner: T,
+            interceptor: F,
+        ) -> QueryClient<InterceptedService<T, F>>
+        where
+            F: tonic::service::Interceptor,
+            T::ResponseBody: Default,
+            T: tonic::codegen::Service<
+                http::Request<tonic::body::BoxBody>,
+                Response = http::Response<
+                    <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
+                >,
+            >,
+            <T as tonic::codegen::Service<
+                http::Request<tonic::body::BoxBody>,
+            >>::Error: Into<StdError> + Send + Sync,
+        {
+            QueryClient::new(InterceptedService::new(inner, interceptor))
+        }
+        /// Compress requests with `gzip`.
+        ///
+        /// This requires the server to support it otherwise it might respond with an
+        /// error.
+        #[must_use]
+        pub fn send_gzip(mut self) -> Self {
+            self.inner = self.inner.send_gzip();
+            self
+        }
+        /// Enable decompressing responses with `gzip`.
+        #[must_use]
+        pub fn accept_gzip(mut self) -> Self {
+            self.inner = self.inner.accept_gzip();
+            self
+        }
+        /// Query the current microtx params
+        pub async fn params(
+            &mut self,
+            request: impl tonic::IntoRequest<super::QueryParamsRequest>,
+        ) -> Result<tonic::Response<super::QueryParamsResponse>, tonic::Status> {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static("/microtx.v1.Query/Params");
+            self.inner.unary(request.into_request(), path, codec).await
+        }
+        /// Get an authoritative fee amount which must be paid on Xfer
+        pub async fn xfer_fee(
+            &mut self,
+            request: impl tonic::IntoRequest<super::QueryXferFeeRequest>,
+        ) -> Result<tonic::Response<super::QueryXferFeeResponse>, tonic::Status> {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static("/microtx.v1.Query/XferFee");
+            self.inner.unary(request.into_request(), path, codec).await
+        }
+    }
+}
 /// MsgXfer A Msg meant to send funds from one Althea network wallet to another,
 /// via an automated device.
 /// SENDER The account sending funds to receiver, must also be the signer of the
@@ -15,6 +149,19 @@ pub struct MsgXfer {
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct MsgXferResponse {
+}
+/// A type for the block's event log, every successful Xfer must create one of
+/// these in the event log
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct EventXfer {
+    #[prost(string, tag="1")]
+    pub sender: ::prost::alloc::string::String,
+    #[prost(string, tag="2")]
+    pub receiver: ::prost::alloc::string::String,
+    #[prost(message, repeated, tag="3")]
+    pub amounts: ::prost::alloc::vec::Vec<cosmos_sdk_proto::cosmos::base::v1beta1::Coin>,
+    #[prost(message, repeated, tag="4")]
+    pub fee: ::prost::alloc::vec::Vec<cosmos_sdk_proto::cosmos::base::v1beta1::Coin>,
 }
 /// Generated client implementations.
 pub mod msg_client {
@@ -100,15 +247,4 @@ pub mod msg_client {
             self.inner.unary(request.into_request(), path, codec).await
         }
     }
-}
-/// Params struct
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Params {
-    #[prost(string, tag="1")]
-    pub param: ::prost::alloc::string::String,
-}
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct GenesisState {
-    #[prost(message, optional, tag="1")]
-    pub params: ::core::option::Option<Params>,
 }
