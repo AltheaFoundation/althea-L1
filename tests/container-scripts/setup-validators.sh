b0c5327b8c140543d0efd6675d353b6695d2e059
Christian Borst <christianoborst@gmail.com>
2023-06-26 11:42:27 -0400

    Remove global min fee in test environment

--- tests/container-scripts/setup-validators.sh
+++ tests/container-scripts/setup-validators.sh
@@ -49,11 +49,10 @@ jq '.app_state.bank.denom_metadata += [{"name": "FOO", "symbol": "FOO", "base": 
 # Link the native coin to the EVM
 jq ".app_state.evm.params.evm_denom=\"${STAKING_TOKEN}\"" /foo-token-genesis.json > /evm-denom-genesis.json
 # Unset the base fee in feemarket
-jq '.app_state.feemarket.params.min_gas_price = "0.000000000000000001"' /evm-denom-genesis.json > /feemarket-gas-price-genesis.json
-
+jq '.app_state.feemarket.params.min_gas_price = "0.000000000000000000"' /evm-denom-genesis.json > /zero-gas-genesis.json
 
 # a 120 second voting period to allow us to pass governance proposals in the tests
-jq '.app_state.gov.voting_params.voting_period = "120s"' /feemarket-gas-price-genesis.json > /edited-genesis.json
+jq '.app_state.gov.voting_params.voting_period = "120s"' /zero-gas-genesis.json > /edited-genesis.json
 
 # rename base denom to aalthea
 sed -i 's/stake/aalthea/g' /edited-genesis.json
