6a65f67809a7591d3622faf4eabb4daccfe54bdb
Christian Borst <christianoborst@gmail.com>
2024-04-17 13:51:24 -0400

    Update solidity test ERC20s to fund known EVM wallets in test env

--- solidity/contracts/TestERC20A.sol
+++ solidity/contracts/TestERC20A.sol
@@ -4,16 +4,37 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // One of three testing coins
 contract TestERC20A is ERC20 {
-	constructor() ERC20("Bitcoin MAX", "MAX") {
-		_mint(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 100000000000000000000000000);
-		_mint(0xc783df8a850f42e7F7e57013759C285caa701eB6, 10000);
-		_mint(0xeAD9C93b79Ae7C1591b1FB5323BD777E86e150d4, 10000);
-		_mint(0xE5904695748fe4A84b40b3fc79De2277660BD1D3, 10000);
-		_mint(0x92561F28Ec438Ee9831D00D1D59fbDC981b762b2, 10000);
-		_mint(0x2fFd013AaA7B5a7DA93336C2251075202b33FB2B, 10000);
-		// this is the EtherBase address for our testnet miner in
-		// tests/assets/ETHGenesis.json so it wil have both a lot
-		// of ETH and a lot of erc20 tokens to test with
-		_mint(0xBf660843528035a5A4921534E156a27e64B231fE, 100000000000000000000000000);
-	}
+    constructor() ERC20("Bitcoin MAX", "MAX") {
+        _mint(
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
+            100000000000000000000000000
+        );
+        _mint(
+            0x37f0C93ae1b73d0F8DbCcEa1514d1434a8D70B3b,
+            100000000000000000000000000
+        );
+        _mint(
+            0x653E44056b6312Db41b31B25c301853e67c5e8C7,
+            100000000000000000000000000
+        );
+        _mint(
+            0xc09B3C4F32D9b2A4D9dE17afCb6b16BD10fE8E61,
+            100000000000000000000000000
+        );
+        _mint(
+            0xf21855F6438B591a5dfDBd3DB32D0502b21d8349,
+            100000000000000000000000000
+        );
+        _mint(
+            0xc09B3C4F32D9b2A4D9dE17afCb6b16BD10fE8E61,
+            100000000000000000000000000
+        );
+        // this is the EtherBase address for our testnet miner in
+        // tests/assets/ETHGenesis.json so it wil have both a lot
+        // of ETH and a lot of erc20 tokens to test with
+        _mint(
+            0xBf660843528035a5A4921534E156a27e64B231fE,
+            100000000000000000000000000
+        );
+    }
 }
