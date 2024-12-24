208adefc37fef3a59c9404b4c04566a01c62c406
Christian Borst <christianoborst@gmail.com>
2024-12-24 09:39:41 -0500

    WIP: proto config cleanup and generation scripts

--- contrib/local/protocgen.sh
+++ contrib/local/protocgen.sh
@@ -12,23 +12,19 @@ if [[ $PATH != *"$GOPATH/bin"* ]]; then
 	exit 1
 fi
 
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+cd proto
+
+proto_dirs=$(find ./althea -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  buf protoc \
-  -I "proto" \
-  -I "third_party/proto" \
-  --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
-
-  # # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
-  buf protoc \
-  -I "proto" \
-  -I "third_party/proto" \
-  --grpc-gateway_out=logtostderr=true,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
-
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package $file &>/dev/null; then
+      echo "Generating gogo proto code for $file"
+      buf generate $file --template buf.gen.gogo.yaml
+    fi
+  done
 done
+
+cd ..
 
 # move proto files to the right places
 cp -r github.com/AltheaFoundation/althea-L1/* ./
