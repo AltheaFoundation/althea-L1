ebb5936385e4f6d9ee90cfeef482f8b0f43f8e82
Christian Borst <christianoborst@gmail.com>
2025-01-03 12:23:13 -0500

    Make two script tests even easier to run

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,5 +2,14 @@
 TEST_TYPE=$1
 set -eu
 
+CONTAINER=$(docker ps | grep althea_test_instance | awk '{print $1}')
+echo "Waiting for container to start before starting the test"
+while [ -z "$CONTAINER" ];
+do
+    CONTAINER=$(docker ps | grep althea_test_instance | awk '{print $1}')
+    sleep 1
+done
+echo "Container started, running tests"
+
 # Run test entry point script
 docker exec althea_test_instance /bin/sh -c "pushd /althea/ && tests/container-scripts/integration-tests.sh 1 $TEST_TYPE"
