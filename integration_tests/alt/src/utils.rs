24f80247b7d2e1d04fdee4eea729ec3d3dd55c22
Christian Borst <christianoborst@gmail.com>
2025-04-18 21:01:14 -0400

    Add many queries and transactions

---
+++ integration_tests/alt/src/utils.rs
@@ -0,0 +1,60 @@
+use std::time::Duration;
+
+use clarity::{Address as EthAddress, PrivateKey};
+use num256::Uint256;
+use web30::client::Web3;
+
+pub async fn approve_erc20s(
+    web30: &Web3,
+    dex_contract: EthAddress,
+    wallet: PrivateKey,
+    base: EthAddress,
+    quote: EthAddress,
+    amount: Uint256,
+) {
+    let wallet_addr = wallet.to_address();
+    if base != EthAddress::default() {
+        let allowance = web30
+            .get_erc20_allowance(base, wallet_addr, dex_contract)
+            .await
+            .expect("Failed to get base ERC20 allowance");
+        if allowance < amount {
+            info!(
+                "Approving DEX to spend {} base token wei",
+                amount.to_string()
+            );
+            web30
+                .erc20_approve(
+                    base,
+                    amount,
+                    wallet,
+                    dex_contract,
+                    Some(Duration::from_secs(20)),
+                    vec![],
+                )
+                .await
+                .expect("Failed to approve DEX to spend base token");
+        }
+    }
+    let allowance = web30
+        .get_erc20_allowance(quote, wallet_addr, dex_contract)
+        .await
+        .expect("Failed to get quote ERC20 allowance");
+    if allowance < amount {
+        info!(
+            "Approving DEX to spend {} quote token wei",
+            amount.to_string()
+        );
+        web30
+            .erc20_approve(
+                quote,
+                amount,
+                wallet,
+                dex_contract,
+                Some(Duration::from_secs(20)),
+                vec![],
+            )
+            .await
+            .expect("Failed to approve DEX to spend base token");
+    }
+}
