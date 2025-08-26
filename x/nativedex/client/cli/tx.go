744264a08a8f65b0bdfb6a83f79fea6eb987d535
Christian Borst <christianoborst@gmail.com>
2025-08-26 12:30:22 -0400

    Fix nativedex proposal cli instructions

--- x/nativedex/client/cli/tx.go
+++ x/nativedex/client/cli/tx.go
@@ -50,13 +50,13 @@ func NewUpgradeProxyProposalCmd() *cobra.Command {
 		Long: `Submit a proposal to upgrade the native DEX contracts with a deployed contract.
 Upon passing, the new contract will be installed as the callpath index on the native DEX contract.
 The proposal details must be supplied via a JSON file.`,
-		Example: fmt.Sprintf(`$ %s tx gov submit-proposal upgrade-proxy <path/to/metadata.json> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
+		Example: fmt.Sprintf(`$ %s tx gov submit-legacy-proposal upgrade-proxy <path/to/metadata.json> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
 
 Where metadata.json contains (example):
 
 {
-	"CallpathAddress": "<hex address>",
-	"CallpathIndex": <uint index>,
+	"callpath_address": "<hex address>",
+	"callpath_index": <uint index>,
 }`, version.AppName,
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -92,12 +92,12 @@ func NewCollectTreasuryProposalCmd() *cobra.Command {
 		Short: "Submit a CollectTreasury proposal",
 		Long: `Submit a proposal to distribute the native DEX protocol take for a single token to the registered 'treasury_' address.
 The proposal details must be supplied via a JSON file.`,
-		Example: fmt.Sprintf(`$ %s tx gov submit-proposal collect-treasury <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
+		Example: fmt.Sprintf(`$ %s tx gov submit-legacy-proposal collect-treasury <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
 
 Where metadata.json contains (example):
 
 {
-	"TokenAddress": "<hex address>",
+	"token_address": "<hex address>",
 }`, version.AppName,
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -138,12 +138,12 @@ func NewSetTreasuryProposalCmd() *cobra.Command {
 		Short: "Submit a SetTreasury proposal",
 		Long: `Submit a proposal to update the 'treasury_' address on the native DEX.
 The proposal details must be supplied via a JSON file.`,
-		Example: fmt.Sprintf(`$ %s tx gov submit-proposal set-treasury <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
+		Example: fmt.Sprintf(`$ %s tx gov submit-legacy-proposal set-treasury <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
 
 Where metadata.json contains (example):
 
 {
-	"TreasuryAddress": "<hex address>",
+	"treasury_address": "<hex address>",
 }`, version.AppName,
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -185,12 +185,12 @@ func NewAuthorityTransferProposalCmd() *cobra.Command {
 		Long: `Submit a proposal to transfer the 'authority_' address on the native DEX, effectively replacing the CrocPolicy contract with another one.
 WARNING: THIS MAY HAVE SEVERE UNINTENDED CONSEQUENCES. ENSURE THE NEW CONTRACT IS COMPATIBLE WITH THIS MODULE BEFORE PROPOSING.
 The proposal details must be supplied via a JSON file.`,
-		Example: fmt.Sprintf(`$ %s tx gov submit-proposal authority-transfer <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
+		Example: fmt.Sprintf(`$ %s tx gov submit-legacy-proposal authority-transfer <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
 
 Where metadata.json contains (example):
 
 {
-	"AuthAddress": "<hex address>",
+	"auth_address": "<hex address>",
 }`, version.AppName,
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -231,12 +231,12 @@ func NewHotPathOpenProposalCmd() *cobra.Command {
 		Short: "Submit a HotPathOpen proposal",
 		Long: `Submit a proposal to enable or disable calling swap() directly on the native DEX.
 The proposal details must be supplied via a JSON file.`,
-		Example: fmt.Sprintf(`$ %s tx gov submit-proposal hot-path-open <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
+		Example: fmt.Sprintf(`$ %s tx gov submit-legacy-proposal hot-path-open <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
 
 Where metadata.json contains (example):
 
 {
-	"Open": "<true/false>",
+	"open": "<true/false>",
 }`, version.AppName,
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -277,12 +277,12 @@ func NewSetSafeModeProposalCmd() *cobra.Command {
 		Short: "Submit a SetSafeMode proposal",
 		Long: `Submit a proposal to lock down the native DEX, or unlock it once it has been locked.
 The proposal details must be supplied via a JSON file.`,
-		Example: fmt.Sprintf(`$ %s tx gov submit-proposal set-safe-mode <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
+		Example: fmt.Sprintf(`$ %s tx gov submit-legacy-proposal set-safe-mode <path/to/metadata.json> <in-safe-mode> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
 
 Where metadata.json contains (example):
 
 {
-	"LockDex": "<true/false>",
+	"lock_dex": "<true/false>",
 }`, version.AppName,
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -322,13 +322,13 @@ func NewTransferGovernanceProposalCmd() *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		Short: "Submit a TransferGovernance proposal",
 		Long:  `Submit a proposal to set the CrocPolicy Ops and Emergency governance roles.`,
-		Example: fmt.Sprintf(`$ %s tx gov submit-proposal transfer-governance <path/to/metadata.json> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
+		Example: fmt.Sprintf(`$ %s tx gov submit-legacy-proposal transfer-governance <path/to/metadata.json> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
 
 Where metadata.json contains (example):
 
 {
-	"Ops": "<hex address>",
-	"Emergency": "<hex address>",
+	"ops": "<hex address>",
+	"emergency": "<hex address>",
 }`, version.AppName,
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -363,13 +363,13 @@ func NewOpsProposalCmd() *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		Short: "Submit an Ops proposal",
 		Long:  `Submit a proposal to perform a non-sudo protocolCmd() call on the native DEX`,
-		Example: fmt.Sprintf(`$ %s tx gov submit-proposal ops <path/to/metadata.json> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
+		Example: fmt.Sprintf(`$ %s tx gov submit-legacy-proposal ops <path/to/metadata.json> --from=<key_or_address> --title=<title> --description=<description> --chain-id=<chain-id> --deposit=<deposit>
 
 Where metadata.json contains (example):
 
 {
-	"Callpath": "3",
-	"CmdArgs": "[<ABI Encoded arguments for the protocolCmd() call>]",
+	"callpath": "3",
+	"cmd_args": "[<ABI Encoded arguments for the protocolCmd() call>]",
 }`, version.AppName,
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
