6ce5a607cf856e62b3d487d0ae764641dc8b0e8b
Christian <christianoborst@gmail.com>
2025-10-22 16:25:37 -0400

    Prepare Cardinal Upgrade
    The placeholder ERC20 addresses have been replaced with the mainnet
    addresses
    The gasfreeErc20InteropTokens are now being set to the mainnet stablecoins
    Upgrade README prepared

--- app/app.go
+++ app/app.go
@@ -133,7 +133,7 @@ import (
 	"github.com/AltheaFoundation/althea-L1/app/ante"
 	altheaappparams "github.com/AltheaFoundation/althea-L1/app/params"
 	"github.com/AltheaFoundation/althea-L1/app/upgrades"
-	"github.com/AltheaFoundation/althea-L1/app/upgrades/example"
+	"github.com/AltheaFoundation/althea-L1/app/upgrades/cardinal"
 	"github.com/AltheaFoundation/althea-L1/app/upgrades/tethys"
 	altheacfg "github.com/AltheaFoundation/althea-L1/config"
 	"github.com/AltheaFoundation/althea-L1/x/erc20"
@@ -1322,7 +1322,7 @@ func (app *AltheaApp) registerStoreLoaders() {
 	// Renamed: []storetypes.StoreRename{{"foo", "bar"}}, example foo to bar rename
 	// Deleted: []string{"bazmodule"}, example deleted bazmodule
 
-	// <name> Group module store loader setup
+	// Tethys
 	if upgradeInfo.Name == tethys.PlanName {
 		// Register the Group and Feegrant modules as new modules that need new stores allocated
 		storeUpgrades := storetypes.StoreUpgrades{
@@ -1333,7 +1333,9 @@ func (app *AltheaApp) registerStoreLoaders() {
 
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
 	}
-	if upgradeInfo.Name == example.TethysToExamplePlanName {
+
+	// Cardinal
+	if upgradeInfo.Name == cardinal.TethysToCardinalPlanName {
 		// Register the nativedex module as a new module, since it was missed in an earlier upgrade
 		storeUpgrades := storetypes.StoreUpgrades{
 			Added:   []string{nativedextypes.StoreKey},
